1b2bc7e3475e977ff067a0667b46b932978e8bc3
Roi Tanimoto <roi@hawaii.edu>
2023-04-06 03:59:55 -1000

    create ejs landing page

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
+app.set('view engine', 'ejs');
+
+
 
 app.get("/", (req, res)=>{
-    res.send("Backend working!");
+    res.render("home");
 });
 
 app.listen(3000, ()=>{
