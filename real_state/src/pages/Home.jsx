765fe64e0027097cddd9cfa3da1fedfea2b583cd
Roi Tanimoto <roi@hawaii.edu>
2023-05-01 09:48:00 -1000

    final touches before due date

--- real_state/src/pages/Home.jsx
+++ real_state/src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+import Container from 'react-bootstrap/Container'
 
 export default function Home() {
   return (
     <div>
-        <h1>Big Island Realty Web App</h1>
+      <Container className='text-center'>
+        <h1>Realty Database Project</h1>
         <h2>CS 421</h2>
-        <h3>Ethan Sick, Kalani Beal, and Roi Tanimoto</h3>
+        <h3>Created By Ethan Sick, Kalani Beale, and Roi Tanimoto</h3>
+          
+      </Container>
 
     </div>
-  )
+    
+
+      
+  );
 }
